1022839232b9e0d269c9b2b0bd7527f3d65d15bb
Wayner <w9jose@gmail.com>
2020-09-21 20:07:37 -0400

    actualizacion de la base de datos
    correcion de algunos variables

--- Bolsa de empleados.sql
+++ Bolsa de empleados.sql
@@ -15,6 +15,10 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
+
+
+CREATE DATABASE `jobweb.db`;
+USE `jobweb.db`;
 --
 -- Table structure for table `categoria`
 --
@@ -26,18 +30,9 @@ CREATE TABLE `categoria` (
   `id` int NOT NULL AUTO_INCREMENT,
   `categoria` varchar(50) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `categoria`
---
-
-LOCK TABLES `categoria` WRITE;
-/*!40000 ALTER TABLE `categoria` DISABLE KEYS */;
-INSERT INTO `categoria` VALUES (1,'Developer');
-/*!40000 ALTER TABLE `categoria` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `compañia`
@@ -55,17 +50,10 @@ CREATE TABLE `compañia` (
   PRIMARY KEY (`id`),
   KEY `idUsuario` (`idUsuario`),
   CONSTRAINT `compañia_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `compañia`
---
 
-LOCK TABLES `compañia` WRITE;
-/*!40000 ALTER TABLE `compañia` DISABLE KEYS */;
-/*!40000 ALTER TABLE `compañia` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `puestotrabajo`
@@ -77,30 +65,23 @@ DROP TABLE IF EXISTS `puestotrabajo`;
 CREATE TABLE `puestotrabajo` (
   `id` int NOT NULL AUTO_INCREMENT,
   `idCompañia` int NOT NULL,
-  `tipo` enum('full-time','part-time','freelance') DEFAULT NULL,
-  `posicion` varchar(100) DEFAULT NULL,
-  `ubicacion` varchar(100) DEFAULT NULL,
+  `tipo` enum('full-time','part-time','freelance') DEFAULT 'freelance',
+  `posicion` varchar(100) NOT NULL,
+  `ubicacion` varchar(100) NOT NULL,
   `idCategoria` int NOT NULL,
-  `descripcion` text,
-  `aplicar` text,
-  `estado` tinyint(1) DEFAULT NULL,
-  `fechaPublicacion` datetime DEFAULT NULL,
+  `descripcion` text NOT NULL,
+  `aplicar` text NOT NULL,
+  `estado` bit(1) DEFAULT 1,
+  `fechaPublicacion` datetime DEFAULT now(),
   PRIMARY KEY (`id`),
   KEY `idCompañia` (`idCompañia`),
   KEY `idCategoria` (`idCategoria`),
   CONSTRAINT `puestotrabajo_ibfk_1` FOREIGN KEY (`idCompañia`) REFERENCES `compañia` (`id`),
   CONSTRAINT `puestotrabajo_ibfk_2` FOREIGN KEY (`idCategoria`) REFERENCES `categoria` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `puestotrabajo`
---
 
-LOCK TABLES `puestotrabajo` WRITE;
-/*!40000 ALTER TABLE `puestotrabajo` DISABLE KEYS */;
-/*!40000 ALTER TABLE `puestotrabajo` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `usuario`
@@ -111,21 +92,14 @@ DROP TABLE IF EXISTS `usuario`;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `usuario` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `username` varchar(50) DEFAULT NULL,
-  `password` varchar(50) DEFAULT NULL,
-  `tipo` enum('administrador','poster','user') DEFAULT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `tipo` enum('administrador','poster','user') DEFAULT 'user',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
---
--- Dumping data for table `usuario`
---
 
-LOCK TABLES `usuario` WRITE;
-/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
-/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
