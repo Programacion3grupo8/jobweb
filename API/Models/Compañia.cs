317747aba5dae1d71faaefa19faedaabdb99be03
anotherjez <anotherjez2@gmail.com>
2020-09-22 17:05:43 -0400

    first commit

---
+++ API/Models/Compañia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Compañia
+    {
+        public int id { get; set; }
+        public string logo { get; set; }
+        public string url { get; set; }
+        public string email { get; set; }
+        public int idUsuario { get; set; }
+    }
+}
