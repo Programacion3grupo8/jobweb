bb26bb4d64742b584aa630a16b52c57faedce6ee
Wayner <w9jose@gmail.com>
2020-09-27 15:59:53 -0400

    complementos

---
+++ Jobweb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jobweb.Startup))]
+namespace Jobweb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
