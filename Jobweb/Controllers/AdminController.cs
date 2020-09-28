ad28b200e17f53bcef9aef84c8dd5b502676fd13
Johan <johanmartinezcustodio@gmail.com>
2020-09-28 07:45:24 +0400

    Metodo Get en api para puesto de trabajo

--- Jobweb/Controllers/AdminController.cs
+++ Jobweb/Controllers/AdminController.cs
@@ -162,10 +162,37 @@ namespace Jobweb.Controllers
             return View();
         }
 
-        public ActionResult Listings()
+        // GET: PuestoTrabajo
+        public async Task<ActionResult> Listings()
         {
-            return View();
+            List<PuestoTrabajo> Users = new List<PuestoTrabajo>();
+
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format  
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Sending request to find web api REST service resource GetAllEmployees using HttpClient  
+                HttpResponseMessage Res = await client.GetAsync("api/v1/PuestoTrabajo");
+
+                //Checking the response is successful or not which is sent using HttpClient  
+                if (Res.IsSuccessStatusCode)
+                {
+                    //Storing the response details recieved from web api   
+                    var PuestosResponse = Res.Content.ReadAsStringAsync().Result;
+
+                    //Deserializing the response recieved from web api and storing into the Employee list  
+                    Users = JsonConvert.DeserializeObject<List<PuestoTrabajo>>(PuestosResponse);
+
+                }
+            }
+            return View(Users);
         }
+
+        
         public ActionResult Settings()
         {
             return View();
